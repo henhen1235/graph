dee35a9c558188b8425821b130d06eefc1c064f7
U-BSD\514039 <514039@SHS-95W2NW3.beaverton.k12.or.us>
2025-06-02 15:26:43 -0700

    almost done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cstring>
+#include <vector>
 
 
 using namespace std;
@@ -8,6 +9,7 @@ using namespace std;
 void displays(int graph[20][20], int size, char templeter[20]);
 void addnode(char name, char templeter[20], int& size);
 int searcher(char search, int size, char templeter[20]);
+void runner(int graph[20][20], char templeter[20], int size, vector<int>& visited, vector<int>& unvisited, vector<int>& shortest, vector<int>& previous, int pos, int distance);
 
 int main(){
 int size = 0;
@@ -39,9 +41,9 @@ char templeter[20];
 	char secondn;
 	int value;
 	cout << "Which two nodes would you like to link? Give the name(it is a single char)" << endl;
-	cout << "Node 1: ";
+	cout << "Node 1 (intial node): ";
 	cin >> firstn;
-	cout << "Node 2: ";
+	cout << "Node 2 (connection node): ";
 	cin >> secondn;
 	cout << "Value: ";
 	cin >> value;
@@ -51,7 +53,7 @@ char templeter[20];
 	  cout << "That Node does not exist" << endl;
 	}
 	graph[pos1][pos2] = value;
-	graph[pos2][pos1] = value;
+	//	graph[pos2][pos1] = value;
 	
       }
       else if(strcmp(i2n, console) == 0){ // add node
@@ -64,10 +66,10 @@ char templeter[20];
 	char firstn;
         char secondn;
         int value;
-        cout << "Which two nodes would you like to delete? Give the name(it is a single char)" << endl;
-        cout << "Node 1: ";
+        cout << "Which two nodes vertex would you like to delete? Give the name(it is a single char)" << endl;
+        cout << "Node 1 (inital node): ";
         cin >> firstn;
-        cout << "Node 2: ";
+        cout << "Node 2 (connection node): ";
         cin >> secondn;
         int pos1 = searcher(firstn, size, templeter);
         int pos2 = searcher(secondn, size, templeter);
@@ -75,7 +77,7 @@ char templeter[20];
           cout << "That Node does not exist" << endl;
         }
         graph[pos1][pos2] = -1;
-        graph[pos2][pos1] = -1;
+	//        graph[pos2][pos1] = -1;
 	
       }
       else if(strcmp(i2n, sear) == 0){// print
@@ -95,6 +97,8 @@ char templeter[20];
 	for(int x = 0; x < size; x++){
 	  graph[x][pos] = graph[x][size - 1];
 	  graph[pos][x] = graph[size -1][x];
+	  graph[x][size-1] = -1;
+	  graph[size-1][x] = -1;
 	}
       	char tem1 = templeter[size-1];
 	templeter[pos] = tem1;
@@ -108,13 +112,62 @@ char templeter[20];
 	break;
       }
       else if(strcmp(i2n, path) == 0){// find 
+      char name;
+      cout << "What node do you want to search from?(it can only be a single char): " << endl;
+      cin >> name;
+      int pos = searcher(name, size, templeter);
+      cout << "starting search" << endl;
+      vector<int> visited;
+      vector<int> unvisited;
+      vector<int> shortest;
+      vector<int> previous;
+
+      for(int x = 0; x < size; x++){
+	unvisited.push_back(x);
+	shortest.push_back(-1);
+	previous.push_back(-1);
+	visited.push_back(-1);
       }
+      cout << "initilization done"<< endl;
+      shortest[pos] = 0;
+      int distance = 0;
+      cout << "starting runner" << endl;
+      runner(graph, templeter, size, visited, unvisited, shortest, previous, pos, distance);
+
+      for(int x = 0; x< size; x++){
+	cout << shortest[x] << endl;
+	
+      }
+      }      
       else{
         cout << "Invalid input options are: (addvertex, addnode, deletevertex, deletenode, qu\
 it, print, find): ";
       }
     }
     return 0;
+}
+
+void runner(int graph[20][20], char templeter[20], int size, vector<int>& visited, vector<int>& unvisited, vector<int>& shortest, vector<int>& previous, int pos, int distance){
+  cout << "running: " << pos << endl;
+  vector<int> storage;
+  int tempsize = 0;
+  for(int x = 0; x< size; x++){
+    if(graph[pos][x] != -1 && visited[x] == -1 && (shortest[x] == -1 || shortest[x] > distance + graph[pos][x])){
+      cout << "found path: " << x << endl;
+      shortest[x] = graph[pos][x];
+      previous[x] = x;
+      storage.push_back(x);
+      tempsize++;
+      }
+    }    
+  visited[pos] = pos;
+  unvisited[pos] = -1;
+
+  for(int x = 0; x< tempsize; x++){
+    if(graph[pos][storage[x]] != -1 && visited[storage[x]] == -1){
+      runner(graph, templeter, size, visited, unvisited, shortest, previous, x, distance + graph[pos][x]);
+    }
+    }
 }
 
 
